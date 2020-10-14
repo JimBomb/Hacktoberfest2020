c2a3fe86338be6acdf07234e928b9f28c54d3af0
JimBomb <44555752+JimBomb@users.noreply.github.com>
2020-10-14 07:08:40 +0530

    Update B.py

--- B.py
+++ B.py
@@ -1,1 +1,31 @@
 
+import tweepy
+import time
+
+auth = tweepy.OAuthHandler('token ')
+auth.set_access_token('access token')
+
+api =tweepy.API(auth,wait_on_rate_limit=True,wait_on_rate_limit_notify=True)
+user = api.me()
+print('Your user name is:\t'+user.screen_name)
+
+#choice=int(input('Enter 1 for getting list of followers\n2 for getting'))
+#for follower in tweepy.Cursor(api.followers).items():
+    #print(follower.name)
+
+search='bayern'
+nrTweets=5
+for tweet in tweepy.Cursor(api.search,search).items(nrTweets):
+    try:
+        print('Tweet liked')
+        tweet.favorite()
+        time.sleep(5)
+    except tweepy.TweepError as e:
+        print(e.reason)
+    except StopIteration:
+        break
+
+
+
+    
+
